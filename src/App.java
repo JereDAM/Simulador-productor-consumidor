e106794a3114e41c85e4bc1f57a924d88fa2c193
JereDAM <usr3593@salesianos-lacuesta.net>
2023-11-14 15:49:49 +0000

    Feat: Se usa solo un productor y un consumidor

--- src/App.java
+++ src/App.java
@@ -7,17 +7,17 @@ public class App {
         Almacen almacen = new Almacen(0, 10);
 
         Granjeros granjero1 = new Granjeros(almacen, "Thanos");
-        Granjeros granjero2 = new Granjeros(almacen,"Faker"); //Faker poque en el minuto 5 tiene un huevo de minions
+        // Granjeros granjero2 = new Granjeros(almacen,"Faker"); //Faker poque en el minuto 5 tiene un huevo de minions
 
         Consumidores consumidor1 = new Consumidores(almacen, "nicocadoAbokado");
-        Consumidores consumidor2 = new Consumidores(almacen, "gordaco");
+        // Consumidores consumidor2 = new Consumidores(almacen, "gordaco");
         System.out.println("Lista de verduras: ");
 
         try {
             granjero1.start();
-            granjero2.start();
+            // granjero2.start();
             consumidor1.start();
-            consumidor2.start();
+            // consumidor2.start();
         } catch (Exception e) {
             System.out.println("Error");
             e.printStackTrace();
